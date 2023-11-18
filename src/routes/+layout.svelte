ba7eb570875b35b2592f58458f21c7d24c599238
Nick Cardoza <48460641+nacardo@users.noreply.github.com>
2023-11-17 18:40:09 -0600

    Start geocode form for single address

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import type { LayoutData } from './$types';
+	import TopAppBar from '@smui/top-app-bar';
+	export let data: LayoutData;
+</script>
+
+<div>
+	<TopAppBar variant="static">
+		<h1>Welcome to the Data and Training Center Geocoder</h1>
+	</TopAppBar>
+
+	<slot />
+	<!-- <footer>
+		<p>Copyright 2023</p>
+	</footer> -->
+</div>
+
+<!-- <style>
+	footer {
+		display: flex;
+		justify-content: center;
+		position: absolute;
+		bottom: 0;	
+		background-color: gray;
+		color: white;
+		height: 20px;
+		width: 100%;
+		padding: 20px;
+	}
+	
+</style> -->
---
+++ src/routes/geocode/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	// import { data } from FormData;
+	import TextField from '@smui/textfield';
+	export let data: PageData;
+
+	let focused = false;
+	let value: string | null = null;
+</script>
+
+<div>
+	<form action="POST">
+		<TextField
+			type="email"
+			updateInvalid
+			label="To"
+			bind:value
+			style="min-width: 250px;"
+			input$autocomplete="email"
+			on:focus={() => (focused = true)}
+			on:blur={() => (focused = false)}
+		></TextField>
+	</form>
+	<pre class="status">Focused: {focused}, Value: {value}</pre>
+</div>
