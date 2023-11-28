d37afb1b72616cf71ca638f652ebb44ffd528efd
Nick Cardoza <48460641+nacardo@users.noreply.github.com>
2023-11-28 17:30:57 -0600

    Make download name reactive

--- src/routes/geocode/bulk/+page.svelte
+++ src/routes/geocode/bulk/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { PageData } from '../bulk/$types';
+	// import { geocodeFile } from '$lib/utils';
 	// import { data } from FormData;
 	import List, { Item, PrimaryText, SecondaryText, Separator, Text } from '@smui/list';
 	import TextField from '@smui/textfield';
@@ -11,10 +12,15 @@
 	let focused = false;
 	// let value: string | null = null;
 	let files: FileList;
+	let is_loading: boolean = false;
+	let geo_res: JSON | Error;
+	let blobURL: string | null = null;
+	let geoFileName: string | null = 'geocoded_file.csv';
+	let errorMsg = null;
 	// let dataFile = null;
 	// let address: { [index: string]: string } = {};
-	let geo_res = doPost;
 	let file_uploaded: boolean = false;
+	let res_received: boolean = false;
 
 	$: if (files) {
 		console.log(files);
@@ -24,37 +30,52 @@
 		}
 	}
 
-	async function doPost() {
-		const dataArray = new FormData();
-		// dataArray.append('data_file', files[0].name);
-		dataArray.append('data_file', files[0]);
-		console.log(dataArray);
-		file_uploaded = true;
-		const res = await fetch('http://127.0.0.1:8000/geocode/file', {
-			method: 'POST',
-			body: dataArray
-		});
-		if (res.ok) {
-			geo_res = await res.json();
-			// return await res.json();
-		} else {
-			// Sometimes the API will fail!
-			throw new Error('Request failed');
+	async function geocodeFile(files: FileList) {
+		try {
+			if (files) {
+				const dataArray = new FormData();
+				dataArray.append('data_file', files[0]);
+				await fetch('http://127.0.0.1:8000/geocode/file', {
+					method: 'POST',
+					body: dataArray
+				})
+					.then((res) => {
+						geoFileName = res.headers.get('content-name');
+						return res.blob();
+					})
+					.then((blob) => {
+						blobURL = URL.createObjectURL(blob);
+					})
+					.catch((error) => {
+						console.log('hit a snag');
+						errorMsg = error;
+						throw new Error('Request failed.', error);
+					});
+			} else {
+				throw new Error('No files selected');
+			}
+		} catch (error) {
+			console.error('Error:', error);
+			return null;
 		}
-		// console.log(geo_shape);
 	}
 
-	// const showFile = (event) => {
-	// 	console.log(event.target.files[0]);
-	// 	console.log(files);
-	// }
+	async function handleFormSubmit(event: Event) {
+		event.preventDefault();
+		file_uploaded = true;
+		is_loading = true;
+		await geocodeFile(files);
+		is_loading = false;
+		res_received = true;
+		// return geo_res;
+	}
 </script>
 
 <div style={'margin-top:20px'}>
-	<form>
+	<form on:submit|preventDefault={handleFormSubmit}>
 		<input class="file-input" bind:files type="file" />
-		<button type="submit" on:click={doPost}>Geocode File</button>
-		<!-- <Button variant="raised" on:click={doPost}>Geocode</Button> -->
+		<!-- <button type="submit">Geocode File</button> -->
+		<Button variant="raised" type="submit">Geocode File</Button>
 	</form>
 	<!-- <form method="POST" action="http://127.0.0.1:8000/geocode/address" enctype="application/json">
 		<input type="text" name="street" />
@@ -66,14 +87,24 @@
 
 	<!-- <pre class="status">Focused: {focused}, Value: {street}</pre> -->
 </div>
-<div>
+<div style="margin-top: 20px;">
 	{#if !file_uploaded}
 		<p>Waiting for file upload...</p>
-	{:else}
+	{:else if file_uploaded && is_loading}
 		<p>Geocoding...</p>
 		<LinearProgress indeterminate />
-		{#await geo_res then geo}
+	{:else if blobURL && !errorMsg}
+		<!-- <p>{JSON.stringify(geo_res)}</p> -->
+		<a href={blobURL} download={geoFileName}>Download Geocoded File</a>
+	{:else if errorMsg}
+		<p>{errorMsg}</p>
+		<!-- 
+		{#await geo_res}
+			<p>Geocoding...</p>
+			<LinearProgress indeterminate />
+		{:then geo}
+			<p>In then block...</p>
 			<p>{JSON.stringify(geo)}</p>
-		{/await}
+		{/await} -->
 	{/if}
 </div>
